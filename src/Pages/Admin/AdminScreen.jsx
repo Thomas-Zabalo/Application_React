2fa1bbd8c11305f8048cc14ee1677fd2ae67f244
Thomas ZABALO <thomas.zabalo@gmail.com>
2024-04-24 09:38:06 +0200

    Ajout gestion admin

--- src/Pages/Admin/AdminScreen.jsx
+++ src/Pages/Admin/AdminScreen.jsx
@@ -22,7 +22,6 @@ export default function Admin() {
                         <AdminCard />
                     </Stack>
                 </Box>
-
             </Container>
         </Box >
     );
---
+++ src/Pages/Admin/Gestion.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Gestion() {
+    return (
+        <div>DetailAdmin</div>
+    )
+}
+
+export default Gestion
